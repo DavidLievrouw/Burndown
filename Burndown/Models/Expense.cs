e396abc00bd09ef18de4ad61fb49c241b00779bd
David Lievrouw <david.lievrouw@recomatics.com>
2024-02-12 14:16:34 +0100

    Move files around.

--- Burndown/Models/Expense.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Burndown.Models;
-
-public class Expense {
-    public DateTime Date { get; set; }
-    public string? Description { get; set; }
-    public decimal Amount { get; set; }
-    public required string[] Tags { get; set; }
-    public string? DestinationType { get; set; }
-}
