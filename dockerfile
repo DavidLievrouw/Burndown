9983b302d38a20377432317dd9f45b2d8e41e8f4
David Lievrouw <david.lievrouw@recomatics.com>
2024-02-12 17:29:16 +0100

    Building the app through WSL does not seem to be working in release mode.

--- dockerfile
+++ dockerfile
@@ -1,16 +1,6 @@
-FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build-env
-
-WORKDIR /app
-
-COPY ./src/Burndown/ ./
-RUN dotnet publish -c Debug -r linux-x64 --self-contained true /p:PublishSingleFile=true /p:EnableCompressionInSingleFile=true /p:PublishReadyToRun=true  -o out
-
 FROM mcr.microsoft.com/dotnet/runtime-deps:8.0-jammy-chiseled-extra
 
 WORKDIR /app
-COPY --from=build-env /app/out .
-
-ENV ASPNETCORE_URLS=http://+:9042
-EXPOSE 9042
+COPY dist/Container/ .
 
 ENTRYPOINT ["./Burndown"]
--- run.ps1
+++ run.ps1
@@ -1,13 +1,28 @@
 $Project = "burndown"
+$Product = "Burndown"
 $Tag = "latest"
 
 $InformationPreference = "Continue"
 
-docker-compose --file docker-compose.yml --project-name $Project down --rmi local --remove-orphans
+$SrcDir = "$PSScriptRoot\src"
+$DistDir = "$PSScriptRoot\dev-dist"
 
-docker build --no-cache --network host -m 1GB -t "$($Project):$($Tag)" -f dockerfile .
+if (Test-Path $DistDir) {
+    Remove-Item $DistDir -Force -Recurse
+}
+
+docker-compose --file docker-compose.yml --project-name $Product.ToLower() down --rmi local --remove-orphans
+
+dotnet publish "$SrcDir/$Product/$Product.csproj" --configuration Release --force --output "$DistDir/Container" --self-contained true --runtime linux-x64 -p:EnvironmentName=Production
+
+docker build --network host -m 1GB -t "$($Product.ToLower()):$($Tag)" -f dockerfile .
+
 if (0 -ne $LastExitCode) {
     throw "Docker build failed with exit code $LastExitCode."
 }
 
-docker-compose --file docker-compose.yml --project-name $Project up --detach
+if (Test-Path $DistDir) {
+    Remove-Item $DistDir -Force -Recurse
+}
+
+docker-compose --file docker-compose.yml --project-name $Product.ToLower() up --detach
