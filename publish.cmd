bdaf27241eb6a7525f76893c036cf1cc987862bd
David Lievrouw <david.lievrouw@recomatics.com>
2024-02-12 15:25:03 +0100

    Add publish script.

---
+++ publish.cmd
@@ -0,0 +1,10 @@
+@echo off
+cls
+SET DIR=%~dp0%
+SET SRCDIR=.\src
+SET DISTDIR=%DIR%\dist
+SET PRODUCT=Burndown
+
+IF EXIST %DISTDIR% RD /S /Q %DISTDIR%
+
+dotnet.exe publish %SRCDIR%\%PRODUCT%\%PRODUCT%.csproj --configuration Release --force --output %DISTDIR%\Container --self-contained false --runtime linux-x64
