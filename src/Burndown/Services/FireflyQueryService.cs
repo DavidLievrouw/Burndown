5c4dd670f3dcb13320b09e425b433cda737ccba1
David Lievrouw <david.lievrouw@recomatics.com>
2024-08-01 17:39:27 +0200

    Make the description autocomplete.

--- src/Burndown/Services/FireflyQueryService.cs
+++ src/Burndown/Services/FireflyQueryService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Globalization;
 using System.Net.Http.Headers;
 using System.Text.Json;
+using System.Web;
 using Burndown.Models;
 
 namespace Burndown.Services;
@@ -12,6 +13,30 @@ internal class FireflyQueryService {
     public FireflyQueryService(AuthorizationService authorizationService, HttpClient httpClient) {
         _authorizationService = authorizationService ?? throw new ArgumentNullException(nameof(authorizationService));
         _httpClient = httpClient;
+    }
+
+    public async Task<IEnumerable<string>> GetTransactionNames(string query) {
+        var accessToken = GetAccessToken();
+
+        var request = new HttpRequestMessage(HttpMethod.Get, "api/v1/autocomplete/transactions?query=" + HttpUtility.UrlEncode(query));
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+        var response = await _httpClient.SendAsync(request);
+
+        if (response.IsSuccessStatusCode) {
+            var content = await response.Content.ReadAsStringAsync();
+            using var doc = JsonDocument.Parse(content);
+
+            return doc.RootElement.EnumerateArray()
+                .Select(e => e.GetProperty("name").GetString() ?? string.Empty)
+                .Distinct()
+                .Where(s => !string.IsNullOrEmpty(s))
+                .OrderBy(s => s)
+                .Take(10)
+                .ToList();
+        }
+        
+        throw new HttpRequestException("Failed to get transaction names from Firefly. " + response.ReasonPhrase);
     }
 
     public async Task<IEnumerable<Account>> GetAccounts() {
